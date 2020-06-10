f9f3b8f2c77829822820681b9d4ee13284e08263
solovova <vsolo0@gmail.com>
2020-06-10 13:19:58 +0300

    v 5.12 delete old code

--- src/main/kotlin/logic/develop/LMDevelopCPUUse.kt
+++ src/main/kotlin/logic/develop/LMDevelopCPUUse.kt
@@ -6,7 +6,7 @@ class LMDevelopCPUUse {
     fun cutoff(oldCPU:Double , text: String):Double {
         val newCpu = Game.cpu.getUsed()
         //println("${(newCpu - oldCPU).toInt()} :$text")
-        println("${(newCpu - oldCPU)} :$text")
+        //println("${(newCpu - oldCPU)} :$text")
         return newCpu
     }
 }
